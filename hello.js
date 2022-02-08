0c6fd5fbd838227c2830b118fb07880f75cd52dd
Sean <seanthewonderful@gmail.com>
2022-02-08 13:20:17 -0700

    Galahad bit

---
+++ hello.js
@@ -0,0 +1,21 @@
+console.log('Hello World')
+
+let firstName = 'Galahad'
+let lastName = 'The Pure'
+let age = 34
+let quest = 'I seek the Holy Grail'
+let color = "Blue"
+let idk = "I don't know tha-"
+let q1 = 'What is your quest?'
+let q2 = 'What is your favorite color?'
+let q3 = 'What is the airspeed velocity of an unlaiden swallow?'
+
+console.log(`${firstName} ${lastName} ` +'has entered')
+
+console.log(q1)
+console.log(quest)
+console.log(q2)
+console.log(color)
+console.log(q3)
+console.log("...")
+console.log(idk)
